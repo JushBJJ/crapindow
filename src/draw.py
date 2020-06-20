e37bb0bdb17519ad15ff55dd2c53380ee4976c30
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2020-06-20 21:17:11 +1000

    more functions into draw.py

--- src/draw.py
+++ src/draw.py
@@ -5,35 +5,44 @@ Supported shapes:
     - Rectangle
 """
 
+from screen import foreground_colours, background_colours
 # Rectangles in this case are automatically unfilled.
 
 
 class Rectangle:
     """ Rectangle class for drawing boxes for the photocopier """
 
-    def __init__(self, scr, width, height):
+    def __init__(self, scr, width, height, x, y, **kwargs):
         self.screen = scr
 
+        # Dimensions
         self.width = width
         self.height = height
-        self.location = []
 
-    def draw(self, x, y, key="#"):
+        # Position
+        self.x = x
+        self.y = y
+
+        # Rectangle Configurations
+        self.foreground = kwargs["foreground"]
+        self.background = kwargs["background"]
+        self.filled = kwargs["filled"]
+        self.char = kwargs["char"]
+
+    def draw(self, x, y):
         """ Draw function using whatever key is specifed """
-
-        self.location = [x, y]
 
         # Draw rectangle.
         for line in range(y, y+self.height+1):
             self.screen.cursor.to_pos(x, line)
 
             if line in (y, y+self.height):
-                self.screen.pre_write(key*(self.width+1))
+                self.screen.pre_write(self.char*(self.width+1))
 
             else:
-                self.screen.pre_write(key)
+                self.screen.pre_write(self.char)
                 self.screen.cursor.to_pos(x+self.width, line)
-                self.screen.pre_write(key)
+                self.screen.pre_write(self.char)
 
         self.screen.flush()
 
@@ -48,14 +57,14 @@ class Rectangle:
             3 --------
         """
 
-        x += self.location[0]
-        y += self.location[1]
+        x += self.x
+        y += self.y
 
         self.screen.cursor.to_pos(x, y)
 
         # Write string in position
         for letter in string:
-            if x == self.location[0]+self.width:
+            if x == self.x+self.width:
                 break
 
             self.screen.write(letter, flush=False)
@@ -66,4 +75,4 @@ class Rectangle:
     def to_pos(self, x, y):
         """ Move cursor relative to the box dimensions """
 
-        self.screen.cursor.to_pos(x+self.location[0], y+self.location[1])
+        self.screen.cursor.to_pos(x+self.x, y+self.y)
